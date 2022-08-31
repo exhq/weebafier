cae8b735026e563d8fb49accef0b5ceece6fed91
echo <91651232+exhq@users.noreply.github.com>
2022-08-31 14:45:26 +0430

    im not lazy >:3

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+import urllib.request as req
+import urllib.error
+import random
+import json
+import tempfile
+import sys
+
+
+if len(sys.argv) < 2:
+    funny = 10
+else:
+    funny = int(sys.argv[1])
+
+funnylinks = []
+funnynames =[]
+after = None
+print("fetching stuff")
+while True:
+    try:
+        with req.urlopen(f"https://api.reddit.com/r/Animewallpaper/new?after={after}") as c:
+            data = json.load(c)
+    except urllib.error.HTTPError as s:
+        continue
+    
+    for i in data["data"]["children"]:
+        if "url_overridden_by_dest" not in i["data"]:
+            continue
+        x = i["data"]["url_overridden_by_dest"]
+        if "gallery" not in x and "Mobile" not in i["data"]["link_flair_text"] and i["data"]["post_hint"] == "image":
+           funnylinks.append(x)
+           funnynames.append(i["data"]["title"])
+    after = data["data"]["after"]
+    if len(funnylinks) > funny:
+        break
+for i,b in enumerate(funnynames):
+    print(i+1 , " - " , b , f"{funnylinks[i]}")
+x = input("choose one(or type \"random\"): ")
+if x == "random":
+    x = random.randint(1, len(funnylinks[i]))
+with req.urlopen(funnylinks[int(x)-1]) as img:
+    with tempfile.NamedTemporaryFile("w+b", delete=False, suffix="." + x.split(".")[-1]) as file:
+        file.write(img.read())
+        os.system(f"feh --bg-scale {file.name}")
+        print("i weebafied you mf")
